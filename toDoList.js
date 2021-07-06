dfb5c99d223bf680bcb7f2f9d0d608740eedda38
Carz Eduardo <eduardovinagre123@gmail.com>
2021-07-06 16:05:28 -0300

    Create menu button methods

--- toDoList.js
+++ toDoList.js
@@ -1,18 +1,43 @@
-const createNewCard = () => {
+const createNewCard = (cardName) => {
+    /* Get Value from Input Form */
+    const value = document.getElementById(cardName).value;
+
+
+    /* Create HTML Structure named with the cardName */
     const newCard = document.createElement("div");
-    newCard.id = "1";
+    newCard.id = value;
+    
     const newList = document.createElement("ul");
-    newList.id = "1";
+    newList.id = value + "-list";
     newCard.appendChild(newList);
 
     const currentCard = document.getElementsByTagName("section");
     currentCard[0].appendChild(newCard);
+
+    /* Add created Card in Selector */
+    const cardSelector = document.getElementById("cardSelector"); 
+    const newSelector = document.createElement("option");
+    newSelector.value = value;
+    newSelector.innerText = value;
+    cardSelector.appendChild(newSelector);
+
+    
 }
 
-const createNewTask = (id) => {
-    const List = document.getElementById(id);
-    const newTask = document.createElement("li");
-    newTask.id = "2";
-    List.appendChild(newTask);
-    newTask.appendChild(document.createTextNode("kkkk eae men"));
+const createNewTask = (task, cardId) => {
+    /* Get new task name and selected cardName */  
+    const value = document.getElementById(task).value;
+    const cardName = document.getElementById(cardId).value;
+    
+    /* Add List Item -> Checks for cardName existence */
+    const List = document.getElementById(cardName);
+    
+    if(List === null){
+        alert("This Card Name Don't Exist.")
+    }
+    else{
+        const newTask = document.createElement("li");
+        List.appendChild(newTask);
+        newTask.appendChild(document.createTextNode(value));
+    }
 }
