33bb6518fcd7ec082c0ff9290459d10e37d19c2b
Carz Eduardo <eduardovinagre123@gmail.com>
2021-07-06 18:28:20 -0300

    Add priority-range and delete task button

--- toDoList.js
+++ toDoList.js
@@ -7,9 +7,15 @@ const createNewCard = (cardName) => {
     const newCard = document.createElement("div");
     newCard.id = value;
     
+    const listTitle = document.createElement("h2");
+    listTitle.innerText = value;
+    newCard.appendChild(listTitle);
+
+
     const newList = document.createElement("ul");
     newList.id = value + "-list";
     newCard.appendChild(newList);
+    newCard.className = 'cardPage';
 
     const currentCard = document.getElementsByTagName("section");
     currentCard[0].appendChild(newCard);
@@ -36,8 +42,25 @@ const createNewTask = (task, cardId) => {
         alert("This Card Name Don't Exist.")
     }
     else{
+        const newDiv = document.createElement("div");
+        newDiv.id = value + '-task';
         const newTask = document.createElement("li");
-        List.appendChild(newTask);
+        const newRange = document.createElement("input");
+        newRange.type = "range";
+        newRange.id = value + "-priority";
+        newRange.min = 1;
+        newRange.max = 5;
+
+        const newDelete = document.createElement("button");
+        newDelete.innerText = 'kkk'
+        console.log(newDelete);
+        newDelete.onclick = () => document.getElementById(newDiv.id).remove();;
+
+        newDiv.appendChild(newDelete);
+        newDiv.appendChild(newTask);
+        newDiv.appendChild(newRange);
+
+        List.appendChild(newDiv);
         newTask.appendChild(document.createTextNode(value));
     }
 }
